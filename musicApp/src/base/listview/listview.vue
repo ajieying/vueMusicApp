8fef83f1985998b6dc7b4021d79c58798340063f
liu.junjie <liu.junjie@58che.com>
2018-04-20 17:51:03 +0800

    完善歌手列表功能

--- musicApp/src/base/listview/listview.vue
+++ musicApp/src/base/listview/listview.vue
@@ -1,5 +1,5 @@
 <template>
-  <scroll class="listview" :data="data" ref="listview">
+  <scroll @scroll="scroll" class="listview" :data="data" ref="listview" :listenScroll = "listenScroll" :probeType="probeType">
     <ul>
       <li v-for="group in data" class="list-group" :key="group.title" ref="listGroup">
         <h2 class="list-group-title">{{group.title}}</h2>
@@ -13,7 +13,7 @@
     </ul>
     <div class="list-shortcut" @touchstart="onShortcutTouchStart" @touchmove.stop.prevent="onShortcutTouchMove">
       <ul>
-        <li v-for="(item, index) in shortcutList" class="item" :key="index" :data-index="index">{{item}}</li>
+        <li v-for="(item, index) in shortcutList" class="item" :key="index" :data-index="index" :class="{'current':currentIndex === index}">{{item}}</li>
       </ul>
     </div>
   </scroll>
@@ -38,12 +38,47 @@ export default {
       })
     }
   },
+  data() {
+    return {
+      scrollY: -1,
+      currentIndex: 0
+    }
+  },
   components: {
     Scroll
   },
   created() {
     // vue中会对data和computed的项目中的值添加getter和setter的监听以作视图层和数据层的变化，此处只需要在两个方法中能回去到该数据，因为this.touch对象不需要监听，所以不添加在data和computed中
     this.touch = {}
+    this.listenScroll = true
+    this.listHeight = []
+    this.probeType = 3
+  },
+  watch: {
+    data() {
+      setTimeout(() => {
+        this._calculateHeight()
+      }, 20)
+    },
+    scrollY(newY) {
+      const listHeight = this.listHeight
+      // 当滚动到顶部 newY>0
+      if (newY > 0) {
+        this.currentIndex = 0
+        return
+      }
+      // 在中间部分滚动 listheight不遍历到最后一个
+      for (let i = 0; i < listHeight.length - 1; i++) {
+        let height1 = listHeight[i]
+        let height2 = listHeight[i + 1]
+        if ((!height2) || ((-newY) >= height1 && (-newY) < height2)) {
+          this.currentIndex = i
+          return
+        }
+      }
+      // 当滚动到底部，且-newY大于最后一个元素的上限
+      this.currentIndex = listHeight.length - 2
+    }
   },
   methods: {
     onShortcutTouchStart(e) {
@@ -61,7 +96,21 @@ export default {
       this._scrollTo(anchorIndex)
     },
     _scrollTo(index) {
-      this.$refs.listview.scrollToElement(this.$refs.listGroup[index])
+      this.$refs.listview.scrollToElement(this.$refs.listGroup[index], 0)// 0 表示动画缓动的时间
+    },
+    scroll(pos) {
+      this.scrollY = pos.y
+    },
+    _calculateHeight() {
+      this.listHeight = []
+      const list = this.$refs.listGroup
+      let height = 0
+      this.listHeight.push(height)
+      for (let i = 0; i < list.length; i++) {
+        let item = list[i]
+        height += item.clientHeight
+        this.listHeight.push(height)
+      }
     }
   }
 }
--- musicApp/src/base/scroll/scroll.vue
+++ musicApp/src/base/scroll/scroll.vue
@@ -18,6 +18,10 @@ export default {
     data: {
       type: Array,
       default: null
+    },
+    listenScroll: {
+      type: Boolean,
+      default: false
     }
   },
   mounted() {
@@ -33,6 +37,11 @@ export default {
       this.scroll = new BScroll(this.$refs.wrapper, {
         probeType: this.probeType,
         click: this.click
+      })
+      if (this.listenScroll) {}
+      let me = this
+      this.scroll.on('scroll', (pos) => {
+        me.$emit('scroll', pos)
       })
     },
     enable() {
